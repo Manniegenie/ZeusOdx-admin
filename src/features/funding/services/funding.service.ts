f3e760762b25258c74a87d17c35bd09a9f744bd3
Manniegenie <Manuelwurld@gmail.com>
2025-10-16 12:12:52 +0100

    0-343

--- src/features/funding/services/funding.service.ts
+++ src/features/funding/services/funding.service.ts
@@ -3,7 +3,7 @@ import type { FundUserRequest, FundUserResponse } from '../types/funding.types';
 
 export async function fundUser(data: FundUserRequest, token: string): Promise<FundUserResponse> {
   const BASE_URL = import.meta.env.VITE_BASE_URL;
-  const res = await axios.post(`${BASE_URL}/fund-user`, data, {
+  const res = await axios.post(`${BASE_URL}/fund/fund-user`, data, {
     headers: {
       Authorization: `Bearer ${token}`,
       'Content-Type': 'application/json',
@@ -14,7 +14,7 @@ export async function fundUser(data: FundUserRequest, token: string): Promise<Fu
 
 export async function deductUser(data: FundUserRequest, token: string): Promise<FundUserResponse> {
   const BASE_URL = import.meta.env.VITE_BASE_URL;
-  const res = await axios.post(`${BASE_URL}/deduct-user`, data, {
+  const res = await axios.post(`${BASE_URL}/fund/deduct-user`, data, {
     headers: {
       Authorization: `Bearer ${token}`,
       'Content-Type': 'application/json',
