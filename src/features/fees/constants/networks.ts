e772343cfb62716df42f046a036d556ebe6c9632
Manniegenie <Manuelwurld@gmail.com>
2025-12-23 12:27:40 +0100

    2300393

--- src/features/fees/constants/networks.ts
+++ src/features/fees/constants/networks.ts
@@ -1,13 +1,12 @@
 export const DEPOSIT_NETWORK_OPTIONS = [
-  'BTC',
-  'ETH',
-  'POL',
-  'BSC',
-  'TRX',
-  'MATIC',
-  'ARBITRUM',
-  'AVAXC',
-  'SOL',
+  'BTC',      // Bitcoin Native
+  'ETH',      // Ethereum (ERC20)
+  'BSC',      // Binance Smart Chain (BEP20)
+  'TRX',      // Tron (TRC20)
+  'MATIC',    // Polygon Native (Replaces 'POL' as a network code)
+  'SOL',      // Solana Native
+  'ARBITRUM', // Arbitrum One
+  'AVAXC',    // Avalanche C-Chain
 ];
 
 
